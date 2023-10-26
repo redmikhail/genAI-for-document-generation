56b39ea05ac55eb19448badb36d3dbe0b4c0f669
Twinkll Sisodia <tsisodia@redhat.com>
2023-10-26 18:05:29 -0400

    Add flask app for endpoints to store the ratings to REDIS DB

---
+++ ui/ratings_api.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import redis
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+@app.route('/store_rating', methods=['POST'])
+def store_rating():
+    try:
+        data = request.get_json()
+        rating = data['rating']
+        
+        # Store the rating in Redis
+        redis_client.rpush('ratings', rating)
+        
+        return jsonify({"message": "Rating stored successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_ratings', methods=['GET'])
+def get_ratings():
+    try:
+        # Retrieve all ratings from Redis
+        ratings = redis_client.lrange('ratings', 0, -1)
+        
+        return jsonify({"ratings": ratings})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
